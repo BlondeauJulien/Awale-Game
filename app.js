1d85a323ab936c2924cfbbc634aa6da09578eca2
Julien Blondeau <julienblondeau@live.fr>
2019-08-13 16:50:01 +0200

    move start game functions in app.js

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 const game = new Game();
 
-document.getElementById('start').addEventListener('click', game.startGame);
+document.getElementById('start').addEventListener('click', startGame);
 
 
+/******************** Start Game Setup ******************/
+
+function startGame() {
+    fillSeedContainer();
+    initializeScores();
+    pickStartingPlayer();
+}
+
+function fillSeedContainer() {
+    let seedContainers = document.querySelectorAll('.seed-container');
+    for(let i = 0; i< seedContainers.length; i++) {
+        seedContainers[i].innerText = '4';
+    }
+}
+
+function initializeScores() {
+    let scores = document.querySelectorAll('.score');
+    for(let i = 0; i< scores.length; i++) {
+        scores[i].innerText = 0;
+    }
+}
+
+function pickStartingPlayer() {
+    let flipACoin = Math.floor(Math.random() * 2);
+
+    if (flipACoin === 0) {
+        game.playerOne.isTurnToPlay = true;
+        game.playerTwo.isTurnToPlay = false;
+    } else {
+        game.playerOne.isTurnToPlay = false;
+        game.playerTwo.isTurnToPlay = true;
+    }
+}
--- game.js
+++ game.js
@@ -9,39 +9,5 @@ class Game {
             score: 0
         }
     }
-
-    /******************** Start Game Setup ******************/
-
-    startGame() {
-        game.fillSeedContainer();
-        game.initializeScores();
-        game.pickStartingPlayer();
-    }
-    
-    fillSeedContainer() {
-        let seedContainers = document.querySelectorAll('.seed-container');
-        for(let i = 0; i< seedContainers.length; i++) {
-            seedContainers[i].innerText = '4';
-        }
-    }
-    
-    initializeScores() {
-        let scores = document.querySelectorAll('.score');
-        for(let i = 0; i< scores.length; i++) {
-            scores[i].innerText = 0;
-        }
-    }
-    
-    pickStartingPlayer() {
-        let flipACoin = Math.floor(Math.random() * 2);
-    
-        if (flipACoin === 0) {
-            game.playerOne.isTurnToPlay = true;
-            game.playerTwo.isTurnToPlay = false;
-        } else {
-            game.playerOne.isTurnToPlay = false;
-            game.playerTwo.isTurnToPlay = true;
-        }
-    }
     
 }
