d1bfa69a3fad6f2893334d189fd9e3277c34da1c
Julien Blondeau <julienblondeau@live.fr>
2019-08-13 18:59:41 +0200

    first draft gameplay functions

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const game = new Game();
 
 document.getElementById('start').addEventListener('click', startGame);
 
+/****** Set Up The Board Game *******/
+
+let playerOneBoardGameSide = [...document.querySelectorAll('.seed-container-p1')]
+let playerTwoBoardGameSide = [...document.querySelectorAll('.seed-container-p2')]
+
+const boardGame = [playerOneBoardGameSide.reverse(), playerTwoBoardGameSide];
+
 
 /******************** Start Game Setup ******************/
 
 function startGame() {
-    fillSeedContainer();
-    initializeScores();
-    pickStartingPlayer();
+    if(game.gameStarted === false) {
+        fillSeedContainer();
+        initializeScores();
+        pickStartingPlayer();
+
+        game.gameStarted = true;
+    }
+
 }
 
 function fillSeedContainer() {
@@ -31,8 +43,33 @@ function pickStartingPlayer() {
     if (flipACoin === 0) {
         game.playerOne.isTurnToPlay = true;
         game.playerTwo.isTurnToPlay = false;
+        document.querySelector('.container-player1').style.background = '#e62d2d';
     } else {
         game.playerOne.isTurnToPlay = false;
         game.playerTwo.isTurnToPlay = true;
+        document.querySelector('.container-player2').style.background = '#313cda';
+    }
+}
+
+
+/******************* Gameplay  ******************************/
+
+let seedContainers = document.querySelectorAll('.seed-container');
+
+for(let i = 0; i<seedContainers.length; i++) {
+    seedContainers[i].addEventListener('click', play);
+}
+
+function play(e) {
+
+
+    if(e.target.classList.contains('seed-container-p1') && game.playerOne.isTurnToPlay === true) {
+        
+
+
+
+
+    } else if (e.target.classList.contains('seed-container-p2') && game.playerTwo.isTurnToPlay === true){
+        console.log(e.target.id)
     }
 }
--- game.js
+++ game.js
@@ -1,9 +1,12 @@
 class Game {
     constructor() {
+        this.gameStarted = false;
+
         this.playerOne = {
             isTurnToPlay: false,
             score: 0
         }
+
         this.playerTwo = {
             isTurnToPlay: false,
             score: 0
